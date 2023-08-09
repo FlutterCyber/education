163c9be715f14e086b3cd49b0fa3c13a54d32e8c
fluttercyber <ronaldrask1@gmail.com>
2023-08-09 22:06:49 +0500

    Initial commit1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:education/intro_page1.dart';
-import 'package:education/liquid_swipe_page.dart';
+import 'package:education/pages/bookmark_page/bookmark_page.dart';
+import 'package:education/pages/bottom_navbar_view.dart';
+import 'package:education/pages/home_page/home_page.dart';
+import 'package:education/pages/intro_pages/liquid_swipe_view.dart';
+import 'package:education/pages/notification_page/notification_page.dart';
+import 'package:education/pages/one_couse_page/one_course_page.dart';
+import 'package:education/pages/profile_page/profile_page.dart';
+import 'package:education/pages/sign_in_up_pages/sign_in_page.dart';
+import 'package:education/pages/sign_in_up_pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
-import 'intro_page2.dart';
-import 'intro_page3.dart';
-import 'intro_page4.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,20 +20,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyLiquidSwipePage(),
+      home: SignInPage(),
       routes: {
-        MyLiquidSwipePage.id: (context) => const MyLiquidSwipePage(),
-        IntroPage1.id: (context) => const IntroPage1(),
-        IntroPage2.id: (context) => const IntroPage2(),
-        IntroPage3.id: (context) => const IntroPage3(),
-        IntroPage4.id: (context) => const IntroPage4(),
+        LiquidSwipeView.id: (context) => LiquidSwipeView(),
+        SignInPage.id: (context) => SignInPage(),
+        SignUpPage.id: (context) => SignUpPage(),
+        BottomNavBarView.id: (context) => BottomNavBarView(),
+        HomePage.id: (context) => HomePage(),
+        OneCoursePage.id: (context) => OneCoursePage(),
+        BookmarkPage.id: (context) => BookmarkPage(),
+        NotificationPage.id: (context) => NotificationPage(),
+        ProfilePage.id: (context) => ProfilePage(),
       },
     );
   }
